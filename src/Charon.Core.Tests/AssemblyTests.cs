ac461cbe22fe9b693fb7b6427d2459b42f2a54b0
Dirk Helbig <helbig.dirk@web.de>
2025-04-14 19:40:50 +0200

    Update AssemblyTests.cs
    Fixed test

--- src/Charon.Core.Tests/AssemblyTests.cs
+++ src/Charon.Core.Tests/AssemblyTests.cs
@@ -8,7 +8,7 @@ namespace Charon.Core.Tests
         [Fact]
         public void Test()
         {
-            Assert.Equal("xunit.execution.dotnet, Version=2.9.2.0, Culture=neutral, PublicKeyToken=8d05b1bb7a6fdb6c", Assembly.GetCallingAssembly().FullName);
+            Assert.Equal("xunit.execution.dotnet, Version=2.9.3.0, Culture=neutral, PublicKeyToken=8d05b1bb7a6fdb6c", Assembly.GetCallingAssembly().FullName);
             Assert.Equal("testhost, Version=15.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", Assembly.GetEntryAssembly()?.FullName);
             Assert.Equal("Charon.Core.Tests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Assembly.GetExecutingAssembly().FullName);
         }
