3df19bc4436478aa15c6897ede7a9cb979123dcc
Monti <dirkhelbig911@gmail.com>
2025-10-06 12:39:47 +0200

    Added sidebar skeleton

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,15 +21,21 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>{children}</Providers>
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            <Providers>{children}</Providers>
+          </main>
+        </SidebarProvider>
       </body>
     </html>
-  );
+  )
 }
---
+++ frontend/update_dependencies.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#  _____ _                           
+# /  __ \ |                          
+# | /  \/ |__   __ _ _ __ ___  _ __  
+# | |   | '_ \ / _` | '__/ _ \| '_ \ 
+# | \__/\ | | | (_| | | | (_) | | | |
+#  \____/_| |_|\__,_|_|  \___/|_| |_|
+#
+# Copyright (c) Dirk Helbig. All rights reserved.
+#
+
+# Stop script on NZEC
+set -e
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+# By default cmd1 | cmd2 returns exit code of cmd2 regardless of cmd1 success
+# This is causing it to fail
+set -o pipefail
+
+echo "Update tool..."
+
+npm install -g npm https-proxy-agent npm-check-updates --force
+npm version
+
+echo "Update package.json..."
+
+ncu --upgrade
+
+echo "Upgrade outdated packages..."
+
+npm install
+
+echo "Finished"
