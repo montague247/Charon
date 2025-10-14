0e7ba216520701ebbc6844f10bd79d0f524c380d
Monti <dirkhelbig911@gmail.com>
2025-10-14 16:04:19 +0200

    Support authorization header in API

--- src/Charon.Api/Program.cs
+++ src/Charon.Api/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,30 @@ builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi("latest");
 builder.Services.AddCors(o => o.AddPolicy("AllowAll", b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+// JWT Authentication hinzufügen
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        /*
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = "DEIN_ISSUER",
+            ValidAudience = "DEINE_AUDIENCE",
+            use [Authorize] to protect endpoints
+        */
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"] ?? "DEIN_SECRET_KEY"))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +46,9 @@ if (app.Environment.IsDevelopment())
 // Middleware
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
+
+app.UseAuthentication(); // JWT Authentifizierung aktivieren
+app.UseAuthorization();
 
 app.MapControllers();
 
