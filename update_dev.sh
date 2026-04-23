3a289144b838bf665083af054eb67c252b4cc92c
Dirk Helbig <dirk.helbig@volkswagen.de>
2026-04-23 19:19:30 +0200

    refactor: enhance update_dev.sh for improved dependency management and cleanup

--- update_dev.sh
+++ update_dev.sh
@@ -19,7 +19,28 @@ set -o pipefail
 
 echo "Updating Charon and dependencies..."
 
-brew update && brew upgrade
+PIP=pip3
+
+scriptdir=$(dirname "$0")
+listsdir="$scriptdir/lists"
+mkdir -p "$listsdir"
+
+echo "Update all python packages..."
+$PIP list --local -o --format=freeze | cut -d = -f 1 | xargs $PIP install -U
+$PIP list --local --format=freeze > "$listsdir/pip_list.txt"
+
+echo "Update pipx apps..."
+pipx upgrade-all
+pipx list > "$listsdir/pipx_list.txt"
+
+echo "Update homebrew..."
+brew update
+brew upgrade
+brew list > "$listsdir/brew_list.txt"
+
+echo "Update all brew casks..."
+brew cask outdated | cut -d " " -f 1 | xargs -n 1 brew cask reinstall
+brew cask list > "$listsdir/brew_cask_list.txt"
 
 if command -v ollama >/dev/null 2>&1; then
     echo "Ollama is available"
@@ -29,5 +50,9 @@ fi
 
 [ -d frontend ] && frontend/update_dependenies.sh
 [ -d src ] && src/update_dependenies.sh
+
+echo "Cleanup..."
+
+brew cleanup --prune=0
 
 echo "Finished updating Charon and dependencies."
